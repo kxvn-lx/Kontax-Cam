5ba4a2ac4a7d899f33763a0057a161e53025f5b3
Kevin Laminto <kevin.laminto@gmail.com>
2020-09-28 19:35:44 +1000

    Feat: App clip now also will not pause background audio

--- KontaxCamAppClip/AppDelegate.swift
+++ KontaxCamAppClip/AppDelegate.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import AVFoundation
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.ambient, options: .mixWithOthers)
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        
         return true
     }
 
